cd66918c8d47aa9c232962f8e4ad51d109328564
Yumiko Shiga <totosmy2718@gmail.com>
2026-08-17 08:04:25 +0800

    feat: タスクCRUD実装 #5

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Task;
+// use 
+// Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,4 +44,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+
 }
